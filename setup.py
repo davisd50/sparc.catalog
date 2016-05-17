94e5f9c9b14e0ced0fdf8cc746afc4b7d2b86118
David Davis <davisd50@gmail.com>
2016-05-17 13:40:32 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,50 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.0.1'
+
+setup(name='sparc.catalog',
+      version=version,
+      description="Data catalog components for the SPARC platform",
+      long_description=open("README.md").read() + "\n" +
+                       open("HISTORY.txt").read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+      ],
+      keywords=['zca'],
+      author='David Davis',
+      author_email='davisd50@gmail.com',
+      url='https://github.com/davisd50/sparc.catalog',
+      download_url = '',
+      license='MIT',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['sparc'],
+      include_package_data=True,
+      package_data = {
+          '': ['*.zcml']
+        },
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'persistent',
+          'transaction',
+          'zope.interface',
+          'zope.component',
+          'sparc.entity',
+          'repoze.catalog'
+          # -*- Extra requirements: -*-
+      ],
+      tests_require=[
+          'sparc.testing'
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
